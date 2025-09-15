2e0b4c00e79b7d22da01b9ecc0e170bbf4a9d899
Aidan <aidanchr123@gmail.com>
2025-09-15 18:36:22 -0500

    Fixed song list loading
    Song list now loads after the page loads, you no longer have to type something into the search bar first

--- loadData.js
+++ loadData.js
@@ -1,16 +1,20 @@
+let songs = {}
+
+document.addEventListener("DOMContentLoaded", () => {
+  loadJsonData();
+});
+
 async function loadJsonData() {
   try {
-    const response = await fetch('songList.json'); // Replace with your file path
+    const response = await fetch("songList.json"); // Replace with your file path
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
     const data = await response.json();
-    songs = data
-    console.log(data); // Your JSON data as a JavaScript object
-} catch (error) {
-    console.error('Error loading JSON:', error);
+  songs = data;
+  createList();
+  console.log(data,"test"); // Your JSON data as a JavaScript object
+  } catch (error) {
+    console.error("Error loading JSON:", error);
+  }
 }
-}
-let songs = {}
-loadJsonData();
-setTimeout(createList(),100)
